7b282d4b587289fb7963ae48b97d427a86107e76
WILIAM MANOEL PATRICIO <wiliam.patricio@alunos.sc.senac.br>
2021-08-02 21:58:40 -0300

    alteração 02

---
+++ TemplateBranco/src/templatebranco/TemplateBranco.java
@@ -0,0 +1,69 @@
+// Forma de escrever comum, somente uma linha
+/*
+Forma
+de
+escrever
+em
+barra
+com
+estrela,
+multiplas
+linhas
+*/
+package templatebranco;
+
+/**
+ *                                                                                      
+ * @author wiliam.patricio;
+ */
+public class TemplateBranco {
+
+    /**
+     * @param args the command line arguments;
+     */
+    public static void main(String[] args) {
+        String str = "Olá Mundo"; // Forma de fazer uma váriavel em forma de string;
+        int z = 6; // Variavel criado!;
+        int x = 7;
+        int w = z + x;
+        int valor1 = 1;
+        int valor2 = 2;
+        
+        System.out.println(str); // Print que vai sair no Output do console;
+        System.out.println("6 + 7 = "  + w); // PrintLn (L minusculo), nesse código é feita uma expressão de 6 + 7;
+        System.out.println(valor1 + valor2); // expressão feita com somente as váriaveis e o 'print';
+        
+        //pratica
+        
+        // Fazendo a lista de variaveis
+        
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        // começando o programa
+        
+        System.out.println("Sistema iniciando...");
+        System.out.println(a + b);
+        System.out.println(a + c);
+        System.out.println(a + d);
+        System.out.println(b + a);
+        System.out.println(b + c);
+        System.out.println(b + d);
+        System.out.println(c + a);
+        System.out.println(c + b);
+        System.out.println(c + d);
+        System.out.println(d + a);
+        System.out.println(d + b);
+        System.out.println(d + c);
+        System.out.println("Sistema fechando...");
+        // Finalizado
+        
+        
+        
+        
+        
+
+    }
+    
+}
